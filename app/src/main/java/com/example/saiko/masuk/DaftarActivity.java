0c916320cb7e2e8eb03cb0f3a5899888eb9e5c6a
khatami99 <sauqikhatami084@gmail.com>
2019-12-11 13:11:22 +0700

    Nyoba register

--- app/src/main/java/com/example/saiko/masuk/DaftarActivity.java
+++ app/src/main/java/com/example/saiko/masuk/DaftarActivity.java
@@ -1,15 +1,35 @@
 package com.example.saiko.masuk;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.saiko.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
 
 public class DaftarActivity extends AppCompatActivity {
+    private EditText inputNama, inputEmail, inputPassword;
+    private Button btnSignUp;
+    private FirebaseAuth auth;
+    private DatabaseReference db;
+    private String nama, email, inputNoHp, inputUsia, inputJenisKel, inputDomisili, uniqueID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +37,82 @@ public class DaftarActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
         getSupportActionBar().hide(); //hide the title bar
         setContentView(R.layout.activity_daftar);
+
+        //Variabel Firebase
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance().getReference();
+
+        //Variabel kolom inputan
+        btnSignUp = (Button) findViewById(R.id.btn_daftar);
+        inputNama = (EditText) findViewById(R.id.et_nama);
+        inputEmail = (EditText) findViewById(R.id.et_email);
+        inputPassword = (EditText) findViewById(R.id.et_kata_sandi);
+        inputNoHp = "";
+        inputUsia = "";
+        inputJenisKel = "";
+        inputDomisili = "";
+        // progressBar = (ProgressBar)
+        uniqueID = UUID.randomUUID().toString();
+
+        //Saat tombol SignUp ditekan
+        btnSignUp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                nama = inputNama.getText().toString().trim();
+                email = inputEmail.getText().toString().trim();
+                String emailu = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
+
+                //Error handler
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(getApplicationContext(), "Mohon masukkan E-mail Anda", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(getApplicationContext(), "Mohon masukkan kata sandi Anda", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(password.length() < 6){
+                    Toast.makeText(getApplicationContext(), "Kata sandi terlalu pendek, mohon masukkan kata sandi yang benar", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //progressBar.setVisibility(View.VISIBLE);
+
+                //Bikin akun
+                auth.createUserWithEmailAndPassword(emailu, password).addOnCompleteListener(DaftarActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //progressBar.setVisibility(View.GONE);
+                        if(!task.isSuccessful()){
+                            Toast.makeText(DaftarActivity.this, "Pendaftaran gagal", Toast.LENGTH_SHORT).show();
+                        } else {
+                            submitData(new DataRegis(nama, email, inputNoHp, inputUsia, inputJenisKel, inputDomisili, uniqueID));
+                            Toast.makeText(DaftarActivity.this, "Pendaftaran berhasil", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(DaftarActivity.this, MasukActivity.class));
+                            finish();
+                        }
+                    }
+                });
+            }
+        });
     }
+
+    private void submitData(DataRegis data){
+        db.child("Data Pengguna").push().setValue(data).addOnSuccessListener(this, new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                inputNama.setText("");
+                inputEmail.setText("");
+                inputPassword.setText("");
+//                inputNoHp.setText("");
+//                inputUsia.setText("");
+//                inputJenisKel.setText("");
+//                inputDomisili.setText("");
+            }
+        });
+    }
+
 
     //Intent ke tampilan Masuk
     public void launchMasuk(View view) {
---
+++ app/src/main/java/com/example/saiko/masuk/DataRegis.java
@@ -0,0 +1,96 @@
+package com.example.saiko.masuk;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.io.Serializable;
+
+@IgnoreExtraProperties
+public class DataRegis implements Serializable {
+    private String nama, email, noHp, usia, jenisKel, domisili, UID;
+    private String key;
+
+    public DataRegis(){
+
+    }
+
+    public String getKey(){
+        return key;
+    }
+
+    public void setKey(String key){
+        this.key = key;
+    }
+
+    public String getNama(){
+        return nama;
+    }
+
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getNoHp(){
+        return noHp;
+    }
+
+    public void setNoHp(String noHp){
+        this.noHp = noHp;
+    }
+
+    public String getUsia(){
+        return usia;
+    }
+
+    public void setUsia(String usia){
+        this.usia = usia;
+    }
+
+    public String getJenisKel(){
+        return jenisKel;
+    }
+
+    public void setJenisKel(String jenisKel){
+        this.jenisKel = jenisKel;
+    }
+
+    public String getDomisili(){
+        return domisili;
+    }
+
+    public void setDomisili(String domisili){
+        this.domisili = domisili;
+    }
+
+    public String getUID() { return key; }
+
+    public void setUID(String UID) { this.UID = UID; }
+
+    @Override
+    public String toString(){
+        return " " + nama + "\n" +
+                " " + email + "\n" +
+                " " + noHp + "\n" +
+                " " + usia + "\n" +
+                " " + jenisKel + "\n" +
+                " " + domisili + "\n" +
+                " " + UID;
+    }
+
+    public DataRegis(String nm, String eml, String nohp, String usi4, String jenisk, String dmsli, String UID){
+        nama = nm;
+        email = eml;
+        noHp = nohp;
+        usia = usi4;
+        jenisKel = jenisk;
+        domisili = dmsli;
+        UID = UID;
+    }
+}
